04687d02468f9891fdb46d29355a83640a52aff0
Leo <leo.kowacevic@gmail.com>
2023-12-11 19:30:06 +0100

    Fix

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ import axios from "axios";
 
 const app = express();
 const port = 3000;
-const URL = "https://secrets-api.appbrewery.com/random"
+const URL = "https://secrets-api.appbrewery.com/random";
 
 app.use(express.static("public"));
 
 app.get("/", async(req, res) =>{
-    const response = await axios.get(URL)
-    const results = response.data
-    console.log(results)
-    res.render("index.ejs",{user: results["username"], secret: results.secret})
+    try{
+        const response = await axios.get(URL);
+        const results = response.data;
+        res.render("index.ejs", {
+            user: results["username"], 
+            secret: results.secret});
+    }
+    catch(error){
+        console.error("Error: ", error.message);
+        res.render("index.ejs", {user: "error", secret: "error"});
+    }
 });
 
-app.listen(port, (req, res) =>{
+app.listen(port, (req, res) => {
     console.log("Listening to port " + port);
 });
